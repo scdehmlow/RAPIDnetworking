93d3d8c4b2779fa654240afc1cb5d99d8178a184
SamMindas <samuel.mindas@valpo.edu>
2017-04-09 18:56:42 -0500

    Add files via upload

--- send_receive.cpp
+++ send_receive.cpp
@@ -1,103 +1,75 @@
+ 
+ 
 /*
-*************README*************
-I have not had a chance to test this yet.
-What this code "should" do is constantly receive information.
-If it receives a packet, it will then immediately send the packet of information to 
-the home node. The send packet function has inputs of the data received in the received packet.
-This can be changed to whatever format, it was just the first thing that came to mind. It then
-sends the data to node 00.
+-----------------README-----------------
+As with the previous version, you need to add this to the makefile in the RF24Mesh/example_RPi
+* library. I obviously haven't tested this since I haven't been around the past few days.
+* (sorry for that by the way). Hopefully this works. If not, I will be trying to locate
+* someone to test with by tomorrow. If anyone tests this before then, let me know if it
+* doesn't work and I will try to fix it before Tuesday at the latest.
 
-This node is setup to be node 03
-
-In order to make this file work, it needs to be in the RF24Network/examples_RPi/ library.
-You must also edit the Makefile in that directory to contrain the name send_receive.cpp on the
-line that has all of the other example's names.
 */
-
+  
+#include "RF24Mesh/RF24Mesh.h"  
 #include <RF24/RF24.h>
 #include <RF24Network/RF24Network.h>
-#include <iostream>
-#include <ctime>
-#include <stdio.h>
-#include <time.h>
-
-int sendPacket(int counter,int ms);
 
 
-/**
- * g++ -L/usr/lib main.cc -I/usr/include -o main -lrrd
- **/
-//using namespace std;
-
-// CE Pin, CSN Pin, SPI Speed
-
-// Setup for GPIO 22 CE and GPIO 25 CSN with SPI Speed @ 1Mhz
-//RF24 radio(RPI_V2_GPIO_P1_22, RPI_V2_GPIO_P1_18, BCM2835_SPI_SPEED_1MHZ);
-
-// Setup for GPIO 22 CE and CE0 CSN with SPI Speed @ 4Mhz
-//RF24 radio(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_4MHZ); 
-
-// Setup for GPIO 22 CE and CE1 CSN with SPI Speed @ 8Mhz
 RF24 radio(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ);  
-
 RF24Network network(radio);
+RF24Mesh mesh(radio,network);
 
-// Address of our node in Octal format
-const uint16_t this_node = 03;
+uint32_t displayTimer=0;
 
-// Address of the other node in Octal format (01,021, etc)
-const uint16_t other_node = 00;
+int main(int argc, char** argv) {
+  
+  // Set the nodeID to 0 for the master node
+  mesh.setNodeID(03);
+  // Connect to the mesh
+  printf("start\n");
+  mesh.begin();
+  radio.printDetails();
 
-const unsigned long interval = 2000; //ms  // How often to send 'hello world to the other unit
-
-unsigned long last_sent;             // When did we last send?
-unsigned long packets_sent;          // How many have we sent already
-
-
-struct payload_t {                  // Structure of our payload
-  unsigned long ms;
-  unsigned long counter;
-};
-
-int main(int argc, char** argv) 
+while(1)
 {
-	// Refer to RF24.h or nRF24L01 DS for settings
+  
+  // Call network.update as usual to keep the network updated
+  mesh.update();
 
-	radio.begin();
-	
-	delay(5);
-	network.begin(/*channel*/ 90, /*node address*/ this_node);
-	radio.printDetails();
-	
-	while(1){
+  // In addition, keep the 'DHCP service' running on the master node so addresses will
+  // be assigned to the sensor nodes
+  mesh.DHCP();
+  
+  
+  // Check for incoming data from the sensors
+  while(network.available()){
+//    printf("rcv\n");
+    RF24NetworkHeader header;
+    network.peek(header);
+    
+    uint32_t dat=0;
+    switch(header.type){
+      // Display the incoming millis() values from the sensor nodes
+      case 'M': network.read(header,&dat,sizeof(dat)); 
+                printf("Rcv %u from 0%o\n",dat,header.from_node);
+                 break;
+      default:  network.read(header,0,0); 
+                printf("Rcv bad type %d from 0%o\n",header.type,header.from_node); 
+                break;
+    }
+  }
+delay(2);
+if(!mesh.write(&displayTimer,'M',sizeof(displayTimer))){
+	if(!mesh.checkConnection()){
+	printf("Renewing Address\n");
+	mesh.renewAddress();	
+  }else{
+	  printf("Send fail, Test OK\n");
+}}else{
+	printf("Send OK: %u\n",displayTimer);}
+return 0;
+}}
 
-		network.update();
-  		while ( network.available() ) {     // Is there anything ready for us?
-    			
-		RF24NetworkHeader header;        // If so, grab it and print it out
-   		payload_t payload;
-  		network.read(header,&payload,sizeof(payload));
-			
-		printf("Received payload # %lu at %lu \n",payload.counter,payload.ms);
-		}		  
-		//sleep(2);
-		delay(2000);
-		sendPacket(payload.counter,payload.ms);
-		//fclose(pFile);
-	}
-
-	return 0;
-}
-
-int sendPacket(int counter,int ms){
-	printf("Sending .. ");
-	payload_t payload = {ms,counter};
-	RF24NetworkHeader header(/*to node*/ other_node);
-	bool ok = network.write(header,&payload,sizeof(payload));
-	if (ok){
-		printf("ok.\n");
-	}else{ 
-      	printf("failed.\n");
-  	}
-	return 0;
-}
+      
+      
+      
