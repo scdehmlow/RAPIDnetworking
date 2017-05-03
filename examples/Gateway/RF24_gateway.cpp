1a0ffd74e212d0fc45adc548c300923f0a2e8fbe
kathleen96 <kathleen.salter@valpo.edu>
2017-05-03 11:40:10 -0500

    Update RF24_gateway.cpp

--- examples/Gateway/RF24_gateway.cpp
+++ examples/Gateway/RF24_gateway.cpp
@@ -17,6 +17,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pb_encode.h>
+#include <pb_decode.h>
 #include "nanopb_RPI.pb.h"
 #include <string.h>
 #include <time.h>
@@ -32,59 +34,63 @@ RF24Network network(radio);
 RF24Mesh mesh(radio,network);
 
 void printtime() {
-        time_t rawtime;
-        struct tm *info;
-        char time_buffer[80];
-        time(&rawtime);
-        info = gmtime(&rawtime);
-        strftime(time_buffer,80,"%FT%T.",info);
-        struct timeval tp;
-        gettimeofday(&tp, NULL);
-        double ut = tp.tv_usec;
-        printf("%s%.00f ",time_buffer,ut);
+    time_t rt;
+    struct tm *info;
+    char time_buffer[80];
+    struct timeval tp;
+    gettimeofday(&tp, NULL);
+    rt = tp.tv_sec;
+    info = gmtime(&rt);
+    strftime(time_buffer,80,"%FT%T.",info);
+    double ut = tp.tv_usec;
+    printf("%s%.6f ",time_buffer,ut);
 }
 
 int main(int argc, char** argv) {
 
-  // Set the nodeID to 0 for the master node
-  mesh.setNodeID(0);
-  // Connect to the mesh
-  mesh.begin();
-
-  //Shaffer
-  uint8_t buffer[64];
-  size_t message_length=sizeof(buffer);
-  SensorData message = SensorData_init_zero;
-  pb_istream_t stream = pb_istream_from_buffer(buffer, message_length);
-
-while(1)
-{
-
-  // Call network.update as usual to keep the network updated
-  mesh.update();
-
-  // In addition, keep the 'DHCP service' running on the master node so addresses will
-    // be assigned to the sensor nodes
-  mesh.DHCP();
+    // Set the nodeID to 0 for the master node
+    mesh.setNodeID(0);
+    // Connect to the mesh
+    mesh.begin();
+    mesh.setChannel(95);
 
 
-  // Check for incoming data from the sensors
-  while(network.available()){
-      printtime();
-      RF24NetworkHeader header;
-      network.peek(header);
-      printf("%i NA %i %o ",radio.getChannel(), mesh.getNodeID(header.from_node, header.from_node);
-       char TCPmsg[512]={};
-      uint8_t dat[64];
-      network.read(header,&dat,message_length);
-      for (int i=0; i < sizeof(dat)/sizeof(dat[0]); i++)
-      {
-          printf("%02X",dat[i]);
-      }
-      cout<<"\n";
+    //Shaffer
+    uint8_t buffer[64];
+    size_t message_length=sizeof(buffer);
+    SensorData message = SensorData_init_zero;
+    pb_istream_t stream = pb_istream_from_buffer(buffer, message_length);
 
-  }
-  delay(2);
-}
-return 0;
+    while(1)
+    {
+
+        // Call network.update as usual to keep the network updated
+        mesh.update();
+
+        // In addition, keep the 'DHCP service' running on the master node so address
+        // be assigned to the sensor nodes
+        mesh.DHCP();
+
+
+        // Check for incoming data from the sensors
+        while(network.available()){
+            printtime();
+            RF24NetworkHeader header;
+            network.peek(header);
+            printf("%i NA %i %o ",radio.getChannel(), mesh.getNodeID(header.from_node), header.from_node);
+            char TCPmsg[512]={};
+            uint8_t dat[64];
+            int m_length;
+            m_length=network.read(header,&dat,message_length);
+            printf(" Message length: %i ", m_length/sizeof(dat[0]));
+            for (int i=0; i < m_length; i++)
+            {
+                printf("%02X",dat[i]);
+            }
+            cout<<"\n";
+
+        }
+        delay(2);
+    }
+    return 0;
 }
